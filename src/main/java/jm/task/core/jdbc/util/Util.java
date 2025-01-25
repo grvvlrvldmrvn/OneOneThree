0013805d8b040a672cf46b470f9dc72a532ccf4b
Ivan Puryakov <puryakov@vk.com>
2025-01-25 07:04:51 +0300

    Fix Util
    Create an exception for connection and close the database

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,13 +9,13 @@ public class Util {
     private static final String USERNAME = "mysql";
     private static final String PASSWORD = "mysql";
 
-    public Connection connection;
+    private final Connection connection;
 
     public Util() {
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseConnectionException("Ошибка при подключении к базе данных", e);
         }
     }
 
@@ -27,7 +27,19 @@ public class Util {
         try {
             connection.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseCloseException("Ошибка при закрытии соединения с базой данных", e);
         }
+    }
+}
+
+class DatabaseConnectionException extends RuntimeException {
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+class DatabaseCloseException extends RuntimeException {
+    public DatabaseCloseException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
